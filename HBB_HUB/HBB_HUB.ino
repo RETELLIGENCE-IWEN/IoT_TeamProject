e6aa15eddfac477b2d8c4cdf56ec44a782d13631
RETELLIGENCE-IWEN <iwinstudioi@gmail.com>
2019-12-03 17:04:08 +0900

    Update HBB_HUB.ino
    2.0

--- HBB_HUB/HBB_HUB.ino
+++ HBB_HUB/HBB_HUB.ino
@@ -1,112 +1,292 @@
 // Retelligence Iwen
 // Isaac Kim
-// Based on default BT example for esp32 provided by Arduino.cc
+// Based on default BT example for esp32 
 // KOREA AEROSPACE UNIV.
 // IoT Team Project
 // Home BlackBox
 
+// HUB
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <esp_now.h>
+#include <WiFi.h>
 
-#include "BluetoothSerial.h"
+// Global copy of slave
+#define NUMSLAVES 20
+esp_now_peer_info_t slaves[NUMSLAVES] = {};
+int SlaveCnt = 0;
 
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
+#define CHANNEL 1
+#define PRINTSCANRESULTS 0
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-BluetoothSerial SerialBT; // BT Handler
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#define ESM_SLEEP      55
+#define ESM_OFF        11
+#define ESM_ON         22
+#define ESM_ACTIVATE   99
+#define ESM_DEACTIVATE 88
+#define ESM_NOISSUE    77
+#define ESM_S_ISSUE    44
 
-const int xpin = 36; // x-axis of the accelerometer
-const int ypin = 39; // y-axis
-const int zpin = 34; // z-axis
+#define ESM_index      10
 
-float G_acc_X = 0;
-float G_acc_Y = 0;
-float G_acc_Z = 0;
-
-float acc_X = 0;
-float acc_Y = 0;
-float acc_Z = 0;
-int   _A_   = 0;
+int MoniteringState = ESM_NOISSUE;
+int SecurityState = ESM_ACTIVATE;
 
 float acc_ThreshHold = 2.6;
 
 bool Security_State = true;  // true : security on
 unsigned char Last_cmd = NULL;
 
-// Device Info
-String DeviceCodeName = "HB01";
-String Device_BT_ADDR;
 String __ = "=";
+String DeviceCodeName = "HUB0";
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-// Resset
-void RessetESM() {
-
-
-  delay(500);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Init ESP Now with fallback
+void InitESPNow() {
+  WiFi.disconnect();
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESPNow Init Success");
+  }
+  else {
+    Serial.println("ESPNow Init Failed");
+    // Retry InitESPNow, add a counte and then restart?
+    // InitESPNow();
+    // or Simply Restart
+    ESP.restart();
+  }
 }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Scan for slaves in AP mode
+void ScanForSlave() {
+  int8_t scanResults = WiFi.scanNetworks();
+  //reset slaves
+  memset(slaves, 0, sizeof(slaves));
+  SlaveCnt = 0;
+  Serial.println("");
+  if (scanResults == 0) {
+    Serial.println("No WiFi devices in AP Mode found");
+  } else {
+    //    Serial.print("Found "); Serial.print(scanResults); Serial.println(" devices ");
+    for (int i = 0; i < scanResults; ++i) {
+      // Print SSID and RSSI for each device found
+      String SSID = WiFi.SSID(i);
+      int32_t RSSI = WiFi.RSSI(i);
+      String BSSIDstr = WiFi.BSSIDstr(i);
+
+      if (PRINTSCANRESULTS) {
+        //        Serial.print(i + 1); Serial.print(": "); Serial.print(SSID); Serial.print(" ["); Serial.print(BSSIDstr); Serial.print("]"); Serial.print(" ("); Serial.print(RSSI); Serial.print(")"); Serial.println("");
+      }
+      delay(10);
+      // Check if the current device starts with `Slave`
+      if (SSID.indexOf("Slave") == 0) {
+        // SSID of interest
+        // Get BSSID => Mac Address of the Slave
+        int mac[6];
+
+        if ( 6 == sscanf(BSSIDstr.c_str(), "%x:%x:%x:%x:%x:%x",  &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5] ) ) {
+          for (int ii = 0; ii < 6; ++ii ) {
+            slaves[SlaveCnt].peer_addr[ii] = (uint8_t) mac[ii];
+          }
+        }
+        slaves[SlaveCnt].channel = CHANNEL; // pick a channel
+        slaves[SlaveCnt].encrypt = 0; // no encryption
+        SlaveCnt++;
+      }
+    }
+  }
+
+  if (SlaveCnt > 0) {
+    Serial.print(SlaveCnt); Serial.println(" Slave(s) found, processing..");
+  } else {
+    Serial.println("No Slave Found, trying again.");
+  }
+
+  // clean up ram
+  WiFi.scanDelete();
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
-// SET UP
-void setup()
-{
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// config AP SSID
+void configDeviceAP() {
+  String Prefix = "Master:";
+  String Mac = WiFi.macAddress();
+  String SSID = Prefix + Mac;
+  String Password = "123456789";
+  bool result = WiFi.softAP(SSID.c_str(), Password.c_str(), CHANNEL, 0);
+  if (!result) {
+    Serial.println("AP Config failed.");
+  } else {
+    Serial.println("AP Config Success. Broadcasting with AP: " + String(SSID));
+  }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Check if the slave is already paired with the master.
+// If not, pair the slave with master
+void manageSlave() {
+  if (SlaveCnt > 0) {
+    for (int i = 0; i < SlaveCnt; i++) {
+      //      Serial.print("Processing: ");
+      for (int ii = 0; ii < 6; ++ii ) {
+        //        Serial.print((uint8_t) slaves[i].peer_addr[ii], HEX);
+        if (ii != 5) Serial.print(":");
+      }
+      Serial.print(" Status: ");
+      // check if the peer exists
+      bool exists = esp_now_is_peer_exist(slaves[i].peer_addr);
+      if (exists) {
+        // Slave already paired.
+        Serial.println("Already Paired");
+      } else {
+        // Slave not paired, attempt pair
+        esp_err_t addStatus = esp_now_add_peer(&slaves[i]);
+        if (addStatus == ESP_OK) {
+          // Pair success
+          Serial.println("Pair success");
+        } else if (addStatus == ESP_ERR_ESPNOW_NOT_INIT) {
+          // How did we get so far!!
+          Serial.println("ESPNOW Not Init");
+        } else if (addStatus == ESP_ERR_ESPNOW_ARG) {
+          Serial.println("Add Peer - Invalid Argument");
+        } else if (addStatus == ESP_ERR_ESPNOW_FULL) {
+          Serial.println("Peer list full");
+        } else if (addStatus == ESP_ERR_ESPNOW_NO_MEM) {
+          Serial.println("Out of memory");
+        } else if (addStatus == ESP_ERR_ESPNOW_EXIST) {
+          Serial.println("Peer Exists");
+        } else {
+          Serial.println("Not sure what happened");
+        }
+        delay(100);
+      }
+    }
+  } else {
+    // No slave found to process
+    Serial.println("No Slave found to process");
+  }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+uint8_t data = 100;
+// send data
+void sendData() {
+  data = SecurityState;
+  for (int i = 0; i < SlaveCnt; i++) {
+    const uint8_t *peer_addr = slaves[i].peer_addr;
+    if (i == 0) { // print only for first slave
+      Serial.print("Sending: ");
+      Serial.println(data);
+    }
+    esp_err_t result = esp_now_send(peer_addr, &data, sizeof(data));
+    //    Serial.print("Send Status: ");
+    if (result == ESP_OK) {
+      //      Serial.println("Success");
+    } else if (result == ESP_ERR_ESPNOW_NOT_INIT) {
+      // How did we get so far!!
+      //      Serial.println("ESPNOW not Init.");
+    } else if (result == ESP_ERR_ESPNOW_ARG) {
+      //      Serial.println("Invalid Argument");
+    } else if (result == ESP_ERR_ESPNOW_INTERNAL) {
+      //      Serial.println("Internal Error");
+    } else if (result == ESP_ERR_ESPNOW_NO_MEM) {
+      //      Serial.println("ESP_ERR_ESPNOW_NO_MEM");
+    } else if (result == ESP_ERR_ESPNOW_NOT_FOUND) {
+      //      Serial.println("Peer not found.");
+    } else {
+      //      Serial.println("Not sure what happened");
+    }
+    delay(100);
+  }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// callback when data is sent from Master to Slave
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  //  Serial.print("Last Packet Sent to: "); Serial.println(macStr);
+  Serial.print("Last Packet Send Status: "); Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// callback when data is recv from Slave
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print("!!! Last Packet Recv from ESM -[ ]- : "); Serial.println(macStr);
+  Serial.print("!!! Last Packet Recv Data ESM -[ ]- : "); Serial.println(*data);
+  Serial.println("");
+
+  MoniteringState = *data;
+
+  
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void setup() {
   Serial.begin(115200);
+  //Set device in STA mode to begin with
+  WiFi.mode(WIFI_AP_STA);
+  // configure device AP mode
+  //  configDeviceAP();
+  Serial.println("HBB_HUB Module Online");
+  // This is the mac address of the Master in Station Mode
+  Serial.print("STA MAC: "); Serial.println(WiFi.macAddress());
+  // Init ESPNow with a fallback logic
+  InitESPNow();
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
 
-  SerialBT.begin(DeviceCodeName); //Bluetooth device name
-  Serial.print("\n\nHBB Security HUB [ ");
-  Serial.print(DeviceCodeName);
-  Serial.println(" ] Online");
+  esp_err_t F = esp_now_register_recv_cb(OnDataRecv);
+  if (F == ESP_OK) {
+    Serial.println("register RECV cb function OK");
+  }
+  else {
+    Serial.println("FUCK YOU  FUCK YOU  FUCK YOU  FUCK YOU  FUCK YOU");
+  }
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  pinMode(3, OUTPUT); // LED OUTPUT
-  pinMode(5, OUTPUT); // LED OUTPUT
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void loop() {
+  // In the loop we scan for slave
+  ScanForSlave();
+  // If Slave is found, it would be populate in `slave` variable
+  // We will check if `slave` is defined and then we proceed further
+  if (SlaveCnt > 0) { // check if slave channel is defined
+    // `slave` is defined
+    // Add slave as peer if it has not been added already
+    manageSlave();
+    // pair success or already paired
+    // Send data to device
+    sendData();
+  } else {
+    // No slave found to process
+  }
 
-
-  // Get Device BT ADDR
-  Device_BT_ADDR = "00:11:22:33:FF:EE";
-
+  // wait for 3seconds to run the logic again
   delay(3000);
 }
-
-
-
-
-
-
-int ReadBT() {
-  if (SerialBT.available()) {
-    char BTcmd = (SerialBT.read());
-    delay(50);
-  }
-
-  // Send Ack
-  return 0;
-}
-
-
-void SecurityAlert(int AlertAxis) {
-  Serial.println("Security Alert!  ");
-  while (!SerialBT.available()) {
-    delay(50);
-    Serial.print("BT Connection not available - ");
-    Serial.println(DeviceCodeName);
-    break;
-  }
-  String State  = "E";
-  String S_Code = "0130"; // *** Requires refactoring
-  String StateMSG = String(DeviceCodeName + __ + Device_BT_ADDR + __ + State + __ + S_Code);
-  //  SerialBT.write(StateMSG);
-  Serial.println(StateMSG);
-  Serial.println("\n");
-}
-
-
-
-
-
-void loop()
-{
-
-
-    
-}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
